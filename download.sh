43b63d3b6315c92481892d08701600115ce41464
Theodore Keloglou <zf@sirodoht.com>
2023-04-08 18:49:51 +0100

    add target dir on download script

--- download.sh
+++ download.sh
@@ -2,8 +2,8 @@
 # This software may be used and distributed according to the terms of the GNU General Public License version 3.
 
 PRESIGNED_URL=""             # replace with presigned url from email
-MODEL_SIZE="7,13,30,65"  # edit this list with the model sizes you wish to download
-TARGET_FOLDER=""             # where all files should end up
+MODEL_SIZE="7B,13B,30B,65B"  # edit this list with the model sizes you wish to download
+TARGET_FOLDER="models"             # where all files should end up
 
 declare -A N_SHARD_DICT
 
